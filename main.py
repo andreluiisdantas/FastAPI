40515115f0b2e86b0ed57a0c95bc37ff30a7b329
andreluiisdantas <andreluissousaa10@gmail.com>
2025-08-28 08:11:12 -0300

    Router

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI, HTTPException, status, Response, Depends
 from models import PersonagemToyStory
 from typing import Optional, Any
+from routes import curso_router, usuario_router
+import requests
 
 app = FastAPI(title = "API dos personagens de Toy Story - Andre Dantas DS18", version = "0.0.1", description = "API feita na aula para aprnder FastAPI") #Variavel para instanciar o FastAPI() e não precisar escrever o FastAPI e sim apenas o app
+
+app.include_router(curso_router.router, tags=["Cursos"])
+app.include_router(usuario_router.router, tags=["Usuarios"])
+
+@app.get("/pokemon/{name}")
+def get_pokemon(name: str):
+    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name}")
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {"message": "Pokemon not found"}
 
 def fake_db():
     try:
@@ -26,51 +39,51 @@ personagens = {
     }
 }
 
-@app.get("/")
-async def raiz():
-    return {"Mensagem": "Hello World!"}  
+# @app.get("/")
+# async def raiz():
+#     return {"Mensagem": "Hello World!"}  
 
-@app.get("/personagens")
-async def get_personagens(db: Any = Depends(fake_db)):
-    return personagens
+# @app.get("/personagens")
+# async def get_personagens(db: Any = Depends(fake_db)):
+#     return personagens
 
-@app.get("/personagens/{personagem_id}", description="Retorna o personagem com um ID especifico", summary="Retorna um personagem")
-async def get_personagem(personagem_id: int):
-    try:
-        personagem = personagens[personagem_id]
-        return personagem
-    except KeyError:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Personagem não encontrado")
+# @app.get("/personagens/{personagem_id}", description="Retorna o personagem com um ID especifico", summary="Retorna um personagem")
+# async def get_personagem(personagem_id: int):
+#     try:
+#         personagem = personagens[personagem_id]
+#         return personagem
+#     except KeyError:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Personagem não encontrado")
     
-@app.post("/personagens", status_code=status.HTTP_201_CREATED)
-async def post_personagem(personagem: Optional[PersonagemToyStory] = None):
-    next_id = len(personagens) + 1
-    personagens[next_id] = personagem
-    del personagem.id
-    return personagem
+# @app.post("/personagens", status_code=status.HTTP_201_CREATED)
+# async def post_personagem(personagem: Optional[PersonagemToyStory] = None):
+#     next_id = len(personagens) + 1
+#     personagens[next_id] = personagem
+#     del personagem.id
+#     return personagem
 
-@app.put("/personagens/{personagem_id}", status_code=status.HTTP_202_ACCEPTED)
-async def put_personagem(personagem_id: int, personagem: PersonagemToyStory):
-    if personagem_id in personagens:
-        personagens[personagem_id] = personagem
-        personagem.id = personagem_id
-        del personagem.id
-        return personagem
-    else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Personagem não encontrado")
+# @app.put("/personagens/{personagem_id}", status_code=status.HTTP_202_ACCEPTED)
+# async def put_personagem(personagem_id: int, personagem: PersonagemToyStory):
+#     if personagem_id in personagens:
+#         personagens[personagem_id] = personagem
+#         personagem.id = personagem_id
+#         del personagem.id
+#         return personagem
+#     else:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Personagem não encontrado")
     
-@app.delete("/personagens/{personagem_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_personagem(personagem_id: int):
-    if personagem_id in personagens:
-        del personagens[personagem_id]
-        return Response(status_code=status.HTTP_204_NO_CONTENT)
-    else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Personagem não encontrado")
+# @app.delete("/personagens/{personagem_id}", status_code=status.HTTP_204_NO_CONTENT)
+# async def delete_personagem(personagem_id: int):
+#     if personagem_id in personagens:
+#         del personagens[personagem_id]
+#         return Response(status_code=status.HTTP_204_NO_CONTENT)
+#     else:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Personagem não encontrado")
     
-@app.get("/calcular")
-async def calcular(a: int, b: int):
-    soma = a + b
-    return {"Resultado": soma}
+# @app.get("/calcular")
+# async def calcular(a: int, b: int):
+#     soma = a + b
+#     return {"Resultado": soma}
 
 if __name__ == "__main__":
     import uvicorn
